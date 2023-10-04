92342370e23b8b94381460da05f12a36759ed68d
fakehyphen <fakehyphen@gmail.com>
2023-10-04 17:59:38 +0330

    create login page and uploades page

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, flash
 from flask import Flask, Response, redirect, url_for, request, session, abort
 from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user
 import config
+import os
+from werkzeug.utils import secure_filename
 
+# upload file config
+UPLOAD_FOLDER = 'uploads/'
+ALLOWED_EXTENSIONS = {'xlsx'}
+
+# app config
 app = Flask("__name__")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # config
 app.config.update(
@@ -32,12 +40,50 @@ class User(UserMixin):
 # create some users with ids 1 to 20       
 user = User(0)
 
+# upload file config page
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 
 # some protected url
 @app.route('/')
 @login_required
 def home():
     return Response("Hello brother")
+
+# show phones number
+@app.route('/phones')
+@login_required
+def show():
+    return Response("<h1>this is phones</h1>")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
